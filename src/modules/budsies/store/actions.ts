fc50f9d6d83126fb82ee3d7eaa0fc43133cb56f7
Kochnov Denis <d.kochnov.optimus@gmail.com>
2022-11-23 18:43:10 +0300

    Refs #27843: remove bulkorder status loading action

--- src/modules/budsies/store/actions.ts
+++ src/modules/budsies/store/actions.ts
@@ -450,25 +450,6 @@ export const actions: ActionTree<BudsiesState, RootState> = {
       throw Error('Error while creating bulk order' + result);
     };
   },
-  async getBulkOrderStatus (context, payload): Promise<BulkOrderStatus> {
-    const url = processURLAddress(`${config.budsies.endpoint}/bulk-orders/status?bulkOrderId=${payload}`);
-
-    const { result, resultCode } = await TaskQueue.execute({
-      url,
-      payload: {
-        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-        mode: 'cors',
-        method: 'GET'
-      },
-      silent: false
-    });
-
-    if (resultCode !== 200) {
-      throw Error('Error while getting bulk order status' + result);
-    }
-
-    return result;
-  },
   async loadBulkOrderInfo ({ commit, state }, payload): Promise<void> {
     const url = processURLAddress(`${config.budsies.endpoint}/bulk-orders/info?bulkOrderId=${payload}`);
 
