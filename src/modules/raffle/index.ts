01c0ff8473a527e160bf07cda786121732960e61
Anton Sylka <antonsylka1004@gmail.com>
2023-08-09 09:50:23 +0700

    Refs #30019: Raffle - fix module naming

--- src/modules/raffle/index.ts
+++ src/modules/raffle/index.ts
@@ -4,21 +4,34 @@ import { StorageManager } from '@vue-storefront/core/lib/storage-manager';
 
 import Ticket from './models/ticket.model';
 import { raffleStore } from './store';
-import { SYNCHRONIZE } from './types/action';
+import * as actions from './types/action';
+import * as getters from './types/getter';
 import { SN_RAFFLE } from './types/store-name';
 import { cacheHandlerFactory } from './helpers/cache-handler.factory';
+import { RAFFLE_MODULE_SYNCED_EVENT_NAME } from './types/event';
+
+import RafflePending from './components/pending.vue';
+import RaffleRegistrationForm from './components/registration-form.vue';
+import RaffleWinner from './components/winner.vue';
 
 export const RaffleModule: StorefrontModule = async function ({ store }) {
   StorageManager.init(SN_RAFFLE);
   store.registerModule(SN_RAFFLE, raffleStore);
 
   if (!isServer) {
-    await store.dispatch(SYNCHRONIZE);
+    await store.dispatch(actions.SYNCHRONIZE);
 
     store.subscribe(cacheHandlerFactory());
   }
 }
 
 export {
-  Ticket
+  actions,
+  getters,
+  Ticket,
+  SN_RAFFLE,
+  RAFFLE_MODULE_SYNCED_EVENT_NAME,
+  RafflePending,
+  RaffleRegistrationForm,
+  RaffleWinner
 }
---
+++ src/modules/raffle/store/getters.ts
@@ -0,0 +1,26 @@
+import { GetterTree } from 'vuex';
+import RootState from '@vue-storefront/core/types/RootState'
+
+import { StoreState } from '../types/store-state.interface';
+import { GET_CURRENT_STATE, GET_IS_SYNCED, GET_LAST_WINNING_TICKETS, GET_PARTICIPANT_DATA, GET_REFERRER_TOKEN } from '../types/getter';
+import ParticipantData from '../models/participant-data.model';
+import CurrentState from '../models/current-state.model';
+import Ticket from '../models/ticket.model';
+
+export const getters: GetterTree<StoreState, RootState> = {
+  [GET_REFERRER_TOKEN] (state): string | undefined {
+    return state.referrerCode;
+  },
+  [GET_PARTICIPANT_DATA] (state): ParticipantData | undefined {
+    return state.participantData;
+  },
+  [GET_CURRENT_STATE] (state): CurrentState | undefined {
+    return state.currentState;
+  },
+  [GET_LAST_WINNING_TICKETS] (state): Ticket[] {
+    return state.lastWinningTickets;
+  },
+  [GET_IS_SYNCED] (state): boolean {
+    return state.isSynced;
+  }
+}
