7088082a31d0a545074f37ff73f2eca6bbc2877e
Anton Sylka <antonsylka1004@gmail.com>
2023-12-20 16:28:12 +0700

    Refs #31326: Storyblok Module - join components into one chunk

--- src/modules/vsf-storyblok-module/components/index.ts
+++ src/modules/vsf-storyblok-module/components/index.ts
@@ -1,14 +1,14 @@
 export const components = {
   debug: () => import('./defaults/Debug.vue'),
-  page: () => import('./defaults/Page.vue'),
-  block: () => import('./defaults/Block.vue'),
+  page: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Page.vue'),
+  block: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Block.vue'),
   category_description: () => import('./defaults/Block.vue'),
   product_description: () => import('./defaults/Block.vue'),
-  block_reference: () => import('./defaults/BlockReference.vue'),
-  grid: () => import('./defaults/Grid.vue'),
-  column: () => import('./defaults/Column.vue'),
-  page_section: () => import('./defaults/PageSection.vue'),
-  tile: () => import('./defaults/Tile.vue')
+  block_reference: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/BlockReference.vue'),
+  grid: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Grid.vue'),
+  column: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Column.vue'),
+  page_section: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/PageSection.vue'),
+  tile: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Tile.vue')
 }
 
 export function add (key: string, component: any, options: any = {}) {
