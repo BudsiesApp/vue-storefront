b9d096dc5613ff89a8599b47a62128cee718d183
Anton Sylka <antonsylka1004@gmail.com>
2023-12-21 12:15:08 +0700

    Refs #31347: Use contenthash for chunk names generation

--- src/modules/vsf-storyblok-module/components/index.ts
+++ src/modules/vsf-storyblok-module/components/index.ts
@@ -1,14 +1,14 @@
 export const components = {
-  debug: () => import('./defaults/Debug.vue'),
-  page: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Page.vue'),
-  block: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Block.vue'),
-  category_description: () => import('./defaults/Block.vue'),
-  product_description: () => import('./defaults/Block.vue'),
-  block_reference: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/BlockReference.vue'),
-  grid: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Grid.vue'),
-  column: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Column.vue'),
-  page_section: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/PageSection.vue'),
-  tile: () => import(/* webpackChunkName: "vsf-storyblok-common-components" */ './defaults/Tile.vue')
+  debug: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/Debug.vue'),
+  page: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/Page.vue'),
+  block: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/Block.vue'),
+  category_description: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/Block.vue'),
+  product_description: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/Block.vue'),
+  block_reference: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/BlockReference.vue'),
+  grid: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/Grid.vue'),
+  column: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/Column.vue'),
+  page_section: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/PageSection.vue'),
+  tile: () => import(/* webpackChunkName: "vsf-storyblok" */ './defaults/Tile.vue')
 }
 
 export function add (key: string, component: any, options: any = {}) {
