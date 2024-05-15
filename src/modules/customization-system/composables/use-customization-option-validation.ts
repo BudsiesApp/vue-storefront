a64c2ed7f7fcbfa0139719d5c941d8b27eaf658d
Anton Sylka <antonsylka1004@gmail.com>
2024-05-15 14:45:01 +0700

    Refs #32174: useCustomizationOptionValidation - fix max value count validation

--- src/modules/customization-system/composables/use-customization-option-validation.ts
+++ src/modules/customization-system/composables/use-customization-option-validation.ts
@@ -1,7 +1,16 @@
-import { computed, Ref } from '@vue/composition-api';
+import { computed, Ref, ref, watch } from '@vue/composition-api';
+import { extend } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+import { ValidationRuleSchema } from 'vee-validate/dist/types/types'
+
 import { getFieldAnchorName } from 'theme/helpers/use-form-validation';
 
 import { Customization } from '../types/customization.interface';
+
+extend('required', {
+  ...required,
+  message: 'The \'{_field_}\' field is required'
+});
 
 export function useCustomizationOptionValidation (customization: Ref<Customization>) {
   const validationRef = computed<string>(() => {
@@ -10,10 +19,63 @@ export function useCustomizationOptionValidation (customization: Ref<Customizati
   });
   const validationRules = computed<Record<string, any>>(() => {
     return {
-      required: customization.value.optionData?.isRequired
-      // max:
+      required: customization.value.optionData?.isRequired,
+      maxValueCount: customization.value.optionData?.maxValuesCount &&
+        customization.value.optionData?.maxValuesCount > 1
+        ? {
+          max: customization.value.optionData.maxValuesCount
+        }
+        : false
     }
   });
+  const validatedValue = ref<string | string[] | undefined>();
+
+  const maxValueCountValidationRule = computed<ValidationRuleSchema>(() => {
+    return {
+      validate: (
+        value: string | string[] | undefined,
+        params: any
+      ): boolean => {
+        validatedValue.value = value;
+
+        if (!params) {
+          return true;
+        }
+
+        if (!value) {
+          return true;
+        }
+
+        return value.length <= params.max;
+      },
+      params: ['max'],
+      message: (
+        field: string,
+        params: Record<string, any>
+      ): string => {
+        if (!validatedValue.value || !params?.max) {
+          return '';
+        }
+
+        if (Array.isArray(validatedValue.value)) {
+          return `Maximum count for '${field}' is ${params.max}`;
+        }
+
+        return `'${field}' maximum length is ${params.max}`;
+      }
+    }
+  });
+
+  function extendMaxValueCountValidationRule (): void {
+    extend('maxValueCount', maxValueCountValidationRule.value);
+  }
+
+  extendMaxValueCountValidationRule();
+
+  watch(
+    maxValueCountValidationRule,
+    () => extendMaxValueCountValidationRule
+  );
 
   return {
     validationRef,
