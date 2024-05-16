8d647f03d285cfc3b0d1db1642bd4b97053dbb1a
Anton Sylka <antonsylka1004@gmail.com>
2024-05-16 14:49:41 +0700

    Refs #32174: option type - remove "EXTRA_PHOTOS" type

--- src/modules/customization-system/types/option-type.ts
+++ src/modules/customization-system/types/option-type.ts
@@ -1,5 +1,4 @@
 export enum OptionType {
   GENERIC = 'd45dca2c-5d57-4ad2-87fb-7ac366eefb7f',
   PRODUCTION_TIME = '38e1e4ce-bdd9-4ae9-936d-efb04759e276',
-  EXTRA_PHOTOS = 'bd95107a-6907-4ece-a2be-463d330caa17' //  is it necessary ?
 }
