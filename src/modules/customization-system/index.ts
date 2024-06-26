810b5acb3e128df2812cbcdfd961edfed6078b00
Anton Sylka <antonsylka1004@gmail.com>
2024-06-26 12:22:04 +0700

    Refs #32409: Production Time Selector - filter "Standard" value from customization state

--- src/modules/customization-system/index.ts
+++ src/modules/customization-system/index.ts
@@ -10,8 +10,10 @@ import { useFilesUpload } from './composables/use-files-upload';
 import { useListWidget } from './composables/use-list-widget';
 import { useOptionValueActions } from './composables/use-option-value-actions';
 import { useOptionValuesPrice } from './composables/use-option-values-price';
+import { useProductionTimeSelectorCustomization } from './composables/use-production-time-selector-customization';
 import { useValuesSort } from './composables/use-values-sort';
 import { useWidgetBusyState } from './composables/use-widget-busy-state';
+import { filterCustomizationState } from './helpers/filter-customization-state';
 import { getCustomizationSystemCartItemThumbnail } from './helpers/get-customization-system-cart-item-thumbnail';
 import { useDefaultValue } from './helpers/use-default-value';
 
@@ -23,6 +25,7 @@ import { isFileUploadValue } from './types/is-file-upload-value.typeguard';
 import { ListWidgetInputType } from './types/list-widget-input-type';
 import { OptionType } from './types/option-type';
 import { OptionValue } from './types/option-value.interface';
+import { PRODUCTION_TIME_SELECTOR_STANDARD_OPTION_VALUE_ID } from './types/production-time-selector-standard-option-value-id';
 import { WidgetOptionShape } from './types/widget-option-shape.type';
 import { WidgetType } from './types/widget-type';
 import { WidgetOptionAlignment } from './types/widget-option-alignment.type';
@@ -36,11 +39,13 @@ export {
   ListWidgetInputType,
   OptionType,
   OptionValue,
+  PRODUCTION_TIME_SELECTOR_STANDARD_OPTION_VALUE_ID,
   WidgetOptionAlignment,
   WidgetOptions,
   WidgetOptionShape,
   WidgetType,
 
+  filterCustomizationState,
   getCustomizationSystemCartItemThumbnail,
   isFileUploadValue,
   useAvailableCustomizations,
@@ -56,6 +61,7 @@ export {
   useListWidget,
   useOptionValueActions,
   useOptionValuesPrice,
+  useProductionTimeSelectorCustomization,
   useValuesSort,
   useWidgetBusyState
 }
