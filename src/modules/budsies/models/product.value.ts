56c17d850518c4351b6e9df97309359b2ef526ba
Anton Sylka <antonsylka1004@gmail.com>
2025-05-07 18:26:35 +0800

    Refs #34466: Plush Keychaing Sample - update product id

--- src/modules/budsies/models/product.value.ts
+++ src/modules/budsies/models/product.value.ts
@@ -40,6 +40,5 @@ export enum ProductValue {
   HAWAIIAN_SHIRTS = '44',
   PHOTO_PORTRAITS = '47',
   TUMBLERS = '46',
-  // TODO: update
-  BULK_PLUSH_KEYCHAIN_SAMPLE = '31',
+  BULK_PLUSH_KEYCHAIN_SAMPLE = '50',
 }
