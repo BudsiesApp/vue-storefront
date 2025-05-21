9aa7679f231f1b874da8346296ee6a3f6caa9acb
Anton Sylka <antonsylka1004@gmail.com>
2025-05-21 13:10:25 +0800

    Refs #34586: Keychain Products - update id values

--- src/modules/budsies/types/bulkorder-quote-product-id.ts
+++ src/modules/budsies/types/bulkorder-quote-product-id.ts
@@ -2,7 +2,8 @@ enum BulkorderQuoteProductId {
   PLUSHIE = 1,
   PILLOW = 2,
   KEYCHAIN = 3,
-  PLUSH_KEYCHAIN = 4
+  ACRYLIC_KEYCHAIN = 4,
+  PLUSH_KEYCHAIN = 5
 }
 
 export default BulkorderQuoteProductId;
